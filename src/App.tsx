2c8c41526c74a5ed1c9c9c4fa858182e363be7fe
Carlos Luis <marincarlosl19@gmail.com>
2024-10-20 23:19:37 -0500

    creacion header Layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,29 @@ import HomePage from "./modules/homeModule/pages/HomePage";
 import PlanPage from "./modules/planModule/pages/planPage/PlanPage";
 import SummaryPage from "./modules/planModule/pages/summaryPage/SummaryPage";
 import AppLayout from "./shared/layouts/appLayout/AppLayout";
+import HeaderLayout from "./shared/layouts/HeaderLayout";
 import HeaderNavigateLayout from "./shared/layouts/HeaderNavigateLayout";
 import StepperLayout from "./shared/layouts/StepperLayout";
 import NotFoundPage from "./shared/pages/notFoundPage/NotFoundPage";
 import PlanProvider from "./shared/providers/PlanProvider";
-import Header from "./components/header/Header";
 
 function App() {
   return (
     <Router>
       <PlanProvider>
         <AppLayout>
-          <Header />
-          <StepperLayout>
-            <HeaderNavigateLayout>
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/plan" element={<PlanPage />} />
-                <Route path="/summary" element={<SummaryPage />} />
-                <Route path="*" element={<NotFoundPage />} />
-              </Routes>
-            </HeaderNavigateLayout>
-          </StepperLayout>
+          <HeaderLayout>
+            <StepperLayout>
+              <HeaderNavigateLayout>
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/plan" element={<PlanPage />} />
+                  <Route path="/summary" element={<SummaryPage />} />
+                  <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+              </HeaderNavigateLayout>
+            </StepperLayout>
+          </HeaderLayout>
         </AppLayout>
       </PlanProvider>
     </Router>
